4ee2ed7283f69bda9c6eac611f4f76605ba86d7e
Emmanuel Dominic <43720837+ManuelDominic@users.noreply.github.com>
2019-09-23 17:42:10 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vanilla-data-structure-review-concepts
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  This is home testing back with vanilla js
+</div>
+`;
