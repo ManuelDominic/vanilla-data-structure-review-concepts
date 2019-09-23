5b53ea2e014c781c3238d4b1446011c28db97457
Emmanuel Dominic <43720837+ManuelDominic@users.noreply.github.com>
2019-09-23 17:43:40 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  This is home testing back with vanilla js
+  This is my home test with vanilla js
 </div>
 `;
